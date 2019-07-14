e73fd53fd53abe7c42f9e95264f0afd5d427c35b
vl-domorad <vlad.domorad@mail.ru>
2019-07-14 22:24:34 +0300

    Completed slider

--- slider.js
+++ slider.js
@@ -1,16 +1,16 @@
-const slide = document.getElementById('sl');
-const left = document.getElementById('left');
-const right = document.getElementById('right');
+const slide = document.getElementById('slider__wrap-id');
+const left = document.getElementById('slider__button-left');
+const right = document.getElementById('slider__button-right');
 let translate = 0;
 
 right.addEventListener('click', function () {
     switch (translate) {
         case 0 :
-            slide.style.transform = `translateX(-33.3%)`;
+            slide.style.transform = `translateX(calc(-33.333%)`;
             translate++;
             break;
         case 1 :
-            slide.style.transform = `translateX(-66.6%)`;
+            slide.style.transform = `translateX(-66.666%)`;
             translate++;
             break;
         case 2 :
@@ -23,7 +23,7 @@ right.addEventListener('click', function () {
 left.addEventListener('click', function () {
     switch (translate) {
         case 0 :
-            slide.style.transform = `translateX(-66.6%)`;
+            slide.style.transform = `translateX(-66.666%)`;
             translate = 2;
             break;
         case 1 :
@@ -31,7 +31,7 @@ left.addEventListener('click', function () {
             translate--;
             break;
         case 2 :
-            slide.style.transform = `translateX(-33.3%)`;
+            slide.style.transform = `translateX(-33.333%)`;
             translate--;
             break;
     }
